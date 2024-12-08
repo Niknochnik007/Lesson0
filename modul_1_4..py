a4a62260fc54044528b37b77ec6b1dbec3f23ad8
Niknochnik007 <110355191+Niknochnik007@users.noreply.github.com>
2024-12-08 13:39:33 +0300

    Add files via upload

---
+++ modul_1_4..py
@@ -0,0 +1,4 @@
+my_string = input(" За какую футбольную команду ты болеешь?")
+print("Привет, за", my_string .upper())
+print("Привет, за", my_string .lower())
+print("Привет, за", my_string .replace("Привет, за" , "Салют, за"))
